9cbed945ad77fd018600dcc608f88926bbcfa2aa
soluke22 <soluke22@gmail.com>
2023-12-21 20:30:49 -0500

    set up raylib vsCode and vs

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include "raylib.h"
+
+const int mapWidth = 800;
+const int mapHeight = 450;
+const int treeSymbol = 1;
+
+int map[mapWidth][mapHeight];
+
+void InitializeMap()
+{
+    for (int x = 0; x < mapWidth; x++)
+    {
+        for (int y = 0; y < mapHeight; y++)
+        {
+            map[x][y] = 0;
+        }
+    }
+}
+
+void TreeSpawner()
+{
+    int posX, posY;
+
+    do
+    {
+        posX = GetRandomValue(0, mapWidth - 1);
+        posY = GetRandomValue(0, mapHeight - 1);
+    } while (!SpotChecker(posX, posY));
+
+    map[posX][posY] = treeSymbol;
+
+    DrawCircle(posX, posY, 5, GREEN);
+}
+
+bool SpotChecker(int posX, int posY)
+{
+    if (posX >= 0 && posX < mapWidth && posY >= 0 && posY < mapHeight)
+    {
+        return map[posX][posY] == 0;
+    }
+
+    return false;
+}
